40e30d1b01f3cbec90de8fad0b8b32a5fdbe2fa7
Revathipriya Mathpathi <24p61a3234@vbithyd.ac.in>
2026-06-20 16:03:43 +0530

    Fix CORS issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL?.split(',') || ['http://localhost:5173'],
+    origin: true,
     credentials: true
   })
 );
